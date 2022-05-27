d5e307f6e7588bf67666bb5d906e2f942721f69f
revlog1c <revlog1c@gmail.com>
2022-05-27 17:30:46 +0800

    增加批量消息长度计算新逻辑

--- example/src/main/java/org/apache/rocketmq/example/batch/SplitBatchProducer.java
+++ example/src/main/java/org/apache/rocketmq/example/batch/SplitBatchProducer.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.sysflag.MessageSysFlag;
 import org.apache.rocketmq.example.NameSrvEnum;
 
 /**
@@ -31,6 +32,7 @@ import org.apache.rocketmq.example.NameSrvEnum;
  * 此外，这一批消息的总大小不应超过4MB。
  */
 public class SplitBatchProducer {
+    private static final String TOPIC = "BatchTest";
 
     public static void main(String[] args) throws Exception {
 
@@ -40,10 +42,9 @@ public class SplitBatchProducer {
         producer.start();
 
         //large batch
-        String topic = "BatchTest";
         List<Message> messages = new ArrayList<>(100 * 1000);
         for (int i = 0; i < 100 * 1000; i++) {
-            messages.add(new Message(topic, "Tag", "OrderID" + i, ("Hello world " + i).getBytes()));
+            messages.add(new Message(TOPIC, "Tag", "OrderID" + i, ("Hello world " + i).getBytes()));
         }
 
         //split the large batch into small ones:
@@ -52,32 +53,33 @@ public class SplitBatchProducer {
             List<Message> listItem = splitter.next();
             producer.send(listItem);
         }
-    }
 
+        producer.shutdown();
+    }
 }
 
-/*class ListSplitter implements Iterator<List<Message>> {
-    private int sizeLimit = 1024 * 1024 * 4;
-    private final List<Message> messages;
-    private int currIndex;
+ /*class ListSplitter implements Iterator<List<Message>> {
+     private int sizeLimit = 1024 * 1024 * 4;
+     private final List<Message> messages;
+     private int currIndex;
 
-    public ListSplitter(List<Message> messages) {
-        this.messages = messages;
-    }
+     public ListSplitter(List<Message> messages) {
+         this.messages = messages;
+     }
 
-    @Override
-    public boolean hasNext() {
-        return currIndex < messages.size();
-    }
+     @Override
+     public boolean hasNext() {
+         return currIndex < messages.size();
+     }
 
-    @Override
-    public List<Message> next() {
-        int nextIndex = currIndex;
-        int totalSize = 0;
-        for (; nextIndex < messages.size(); nextIndex++) {
-            Message message = messages.get(nextIndex);
-            int tmpSize = message.getTopic().length() + message.getBody().length;
-            Map<String, String> properties = message.getProperties();
+     @Override
+     public List<Message> next() {
+         int nextIndex = currIndex;
+         int totalSize = 0;
+         for (; nextIndex < messages.size(); nextIndex++) {
+             Message message = messages.get(nextIndex);
+             int tmpSize = message.getTopic().length() + message.getBody().length;
+}            Map<String, String> properties = message.getProperties();
             for (Map.Entry<String, String> entry : properties.entrySet()) {
                 tmpSize += entry.getKey().length() + entry.getValue().length();
             }
@@ -104,16 +106,21 @@ public class SplitBatchProducer {
     }*/
 
 class ListSplitter implements Iterator<List<Message>> {
-    private final int SIZE_LIMIT = 1000 * 1000;
+    private final int SIZE_LIMIT = 1000 * 1000 * 2;
     private final List<Message> messages;
     private int currIndex;
+
     public ListSplitter(List<Message> messages) {
         this.messages = messages;
     }
-    @Override public boolean hasNext() {
+
+    @Override
+    public boolean hasNext() {
         return currIndex < messages.size();
     }
-    @Override public List<Message> next() {
+
+    @Override
+    public List<Message> next() {
         int startIndex = getStartIndex();
         int nextIndex = startIndex;
         int totalSize = 0;
@@ -130,17 +137,20 @@ class ListSplitter implements Iterator<List<Message>> {
         currIndex = nextIndex;
         return subList;
     }
+
     private int getStartIndex() {
         Message currMessage = messages.get(currIndex);
         int tmpSize = calcMessageSize(currMessage);
-        while(tmpSize > SIZE_LIMIT) {
+        // 如果单条消息超过阈值，把这条跳过，避免程序报错
+        while (tmpSize > SIZE_LIMIT) {
             currIndex += 1;
             Message message = messages.get(currIndex);
             tmpSize = calcMessageSize(message);
         }
         return currIndex;
     }
-    private int calcMessageSize(Message message) {
+
+    private int calcMessageSizeOld(Message message) {
         int tmpSize = message.getTopic().length() + message.getBody().length;
         Map<String, String> properties = message.getProperties();
         for (Map.Entry<String, String> entry : properties.entrySet()) {
@@ -150,8 +160,41 @@ class ListSplitter implements Iterator<List<Message>> {
         return tmpSize;
     }
 
+    private int calcMessageSize(Message msg) {
+        Map<String, String> properties = msg.getProperties();
+        int propLength = 0;
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            propLength += entry.getKey().length() + entry.getValue().length();
+        }
+        return calMsgLength(0, msg.getBody().length, msg.getTopic().length(),propLength);
+    }
+
     @Override
     public void remove() {
         throw new UnsupportedOperationException("Not allowed to remove");
+    }
+
+    protected static int calMsgLength(int sysFlag, int bodyLength, int topicLength, int propertiesLength) {
+        int bornhostLength = (sysFlag & MessageSysFlag.BORNHOST_V6_FLAG) == 0 ? 8 : 20;
+        int storehostAddressLength = (sysFlag & MessageSysFlag.STOREHOSTADDRESS_V6_FLAG) == 0 ? 8 : 20;
+        final int msgLen = 4 //TOTALSIZE
+                + 4 //MAGICCODE
+                + 4 //BODYCRC
+                + 4 //QUEUEID
+                + 4 //FLAG
+                + 8 //QUEUEOFFSET
+                + 8 //PHYSICALOFFSET
+                + 4 //SYSFLAG
+                + 8 //BORNTIMESTAMP
+                + bornhostLength //BORNHOST
+                + 8 //STORETIMESTAMP
+                + storehostAddressLength //STOREHOSTADDRESS
+                + 4 //RECONSUMETIMES
+                + 8 //Prepared Transaction Offset
+                + 4 + (bodyLength > 0 ? bodyLength : 0) //BODY
+                + 1 + topicLength //TOPIC
+                + 2 + (propertiesLength > 0 ? propertiesLength : 0) //propertiesLength
+                + 0;
+        return msgLen;
     }
 }
